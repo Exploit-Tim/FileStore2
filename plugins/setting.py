a47e05a46e9b2a2cca9f397a65ed00854835b35a
INCREDIBLE POSISI <87313549+bulanbintang69@users.noreply.github.com>
2025-05-23 02:28:07 +0700

    Update setting.py

--- plugins/setting.py
+++ plugins/setting.py
@@ -9,6 +9,7 @@ async def settings_command(client: Client, message: Message):
         [InlineKeyboardButton("Daftar Admin", callback_data="daftar_admin")],
         [InlineKeyboardButton("Daftar Fsub", callback_data="daftar_fsub")],
         [InlineKeyboardButton("Mode Fsub", callback_data="Mode_fsub")],
+        [InlineKeyboardButton("Time Delete", callback_data="time_delete")],
         [InlineKeyboardButton("Set Welcome", callback_data="set_welcome")],
         [InlineKeyboardButton("Set Force Message", callback_data="set_force_msg")],
         [InlineKeyboardButton("Tutup", callback_data="close")],
