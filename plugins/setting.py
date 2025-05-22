03da5419d55fd9c52617a1ae16271dabbb1fa6b8
INCREDIBLE POSISI <87313549+bulanbintang69@users.noreply.github.com>
2025-05-22 14:12:06 +0700

    Update setting.py

--- plugins/setting.py
+++ plugins/setting.py
@@ -1,7 +1,8 @@
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery
+from bot import Bot
 
-@Client.on_message(filters.command(["setting"]))
+@Bot.on_message(filters.command('settings') & admin)
 async def setting_menu(client, message: Message):
     keyboard = [
         [InlineKeyboardButton("Daftar Admin", callback_data="daftar_admin")],
@@ -14,22 +15,22 @@ async def setting_menu(client, message: Message):
         reply_markup=InlineKeyboardMarkup(keyboard)
     )
 
-@Client.on_callback_query(filters.regex("daftar_admin"))
+@Bot.on_callback_query(filters.regex("daftar_admin"))
 async def daftar_admin(client, callback_query: CallbackQuery):
     await callback_query.message.edit_text("Fitur Daftar Admin")
     await callback_query.answer("Daftar Admin")
 
-@Client.on_callback_query(filters.regex("set_welcome"))
+@Bot.on_callback_query(filters.regex("set_welcome"))
 async def set_welcome(client, callback_query: CallbackQuery):
     await callback_query.message.edit_text("Fitur Set Welcome")
     await callback_query.answer("Set Welcome")
 
-@Client.on_callback_query(filters.regex("set_force_msg"))
+@Bot.on_callback_query(filters.regex("set_force_msg"))
 async def set_force_msg(client, callback_query: CallbackQuery):
     await callback_query.message.edit_text("Fitur Set Force Message")
     await callback_query.answer("Set Force Message")
 
-@Client.on_callback_query(filters.regex("close"))
+@Bot.on_callback_query(filters.regex("close"))
 async def close(client, callback_query: CallbackQuery):
     await callback_query.message.delete()
     await callback_query.answer("Menu Ditutup")
