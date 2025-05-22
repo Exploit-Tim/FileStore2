a5e46735aee60d00881a24f46557009ac0d74b82
INCREDIBLE POSISI <87313549+bulanbintang69@users.noreply.github.com>
2025-05-23 03:46:13 +0700

    Update setting.py

--- plugins/setting.py
+++ plugins/setting.py
@@ -6,13 +6,26 @@ from bot import Bot
 @Bot.on_message(filters.command("settings"))
 async def settings_command(client: Client, message: Message):
     keyboard = [
-        [InlineKeyboardButton("Daftar Admin", callback_data="daftar_admin")],
-        [InlineKeyboardButton("Daftar Fsub", callback_data="daftar_fsub")],
-        [InlineKeyboardButton("Mode Fsub", callback_data="Mode_fsub")],
-        [InlineKeyboardButton("Time Delete", callback_data="time_delete")],
-        [InlineKeyboardButton("Server Info", callback_data="server_info")],
-        [InlineKeyboardButton("Set Welcome", callback_data="set_welcome")],
-        [InlineKeyboardButton("Set Force Message", callback_data="set_force_msg")],
-        [InlineKeyboardButton("Tutup", callback_data="close")],
+        [InlineKeyboardButton("ᴀᴅᴍɪɴ", callback_data="daftar_admin")],
+        [
+            InlineKeyboardButton("ғsᴜʙ", callback_data="daftar_fsub"),
+            InlineKeyboardButton("ᴍᴏᴅᴇ", callback_data="Mode_fsub"),
+        ],
+        [
+            InlineKeyboardButton("ᴅʙ ɪᴅ", callback_data="db_id"),
+            InlineKeyboardButton("ᴅʙ ᴜʀʟ", callback_data="db_url"),
+        ],
+        [InlineKeyboardButton("ᴘʀᴏᴛᴇᴄᴛ", callback_data="protect")],
+        [
+            InlineKeyboardButton("ᴛɪᴍᴇ", callback_data="time_delete"),
+            InlineKeyboardButton("sᴇʀᴠᴇʀ", callback_data="server_info"),
+        ],
+        [
+            InlineKeyboardButton("ᴍᴇssᴀɢᴇ", callback_data="set_force_msg"),
+            InlineKeyboardButton("ᴘɪᴄᴛ", callback_data="set_welcome"),
+        ],
+        [InlineKeyboardButton("ᴄᴜsᴛᴏᴍ ᴄᴀᴘᴛɪᴏɴ", callback_data="custom_caption")],
+        [InlineKeyboardButton("ᴋᴏɴᴛᴇɴ", callback_data="konten")],
+        [InlineKeyboardButton("ᴛᴜᴛᴜᴘ", callback_data="close")],
     ]
-    await message.reply_text("Menu Setting", reply_markup=InlineKeyboardMarkup(keyboard))
+    await message.reply_text("📋 <b>Menu Settings</b>", reply_markup=InlineKeyboardMarkup(keyboard))
