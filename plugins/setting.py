7d5b6ff89b4c3556f12b7ba5ed26bf01da07393f
INCREDIBLE POSISI <87313549+bulanbintang69@users.noreply.github.com>
2025-05-22 14:45:02 +0700

    Update setting.py

--- plugins/setting.py
+++ plugins/setting.py
@@ -3,15 +3,15 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 from database.database import db
 from bot import Bot
 
-@Bot.on_callback_query(filters.regex("settings"))
-async def settings(client: Client, callback_query: CallbackQuery):
+@Bot.on_message(filters.command("settings"))
+async def settings_command(client: Client, message: Message):
     keyboard = [
         [InlineKeyboardButton("Daftar Admin", callback_data="daftar_admin")],
         [InlineKeyboardButton("Set Welcome", callback_data="set_welcome")],
         [InlineKeyboardButton("Set Force Message", callback_data="set_force_msg")],
         [InlineKeyboardButton("Tutup", callback_data="close")],
     ]
-    await callback_query.message.edit_text("Menu Setting", reply_markup=InlineKeyboardMarkup(keyboard))
+    await message.reply_text("Menu Setting", reply_markup=InlineKeyboardMarkup(keyboard))
 
 @Bot.on_callback_query(filters.regex("daftar_admin"))
 async def daftar_admin(client: Client, callback_query: CallbackQuery):
